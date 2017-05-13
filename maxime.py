3c820ac0f3d655a1cacabc1e96a2fbb09da04bec
Grant Cohoe <grant@grantcohoe.com>
2017-05-13 14:48:15 -0400

    Added listening and connection state

--- maxime.py
+++ maxime.py
@@ -4,7 +4,9 @@ import dbus
 import ConfigParser
 import os
 import argparse
+import logging
 from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
 
 class App:
     """
@@ -33,9 +35,18 @@ class App:
 
         parser = argparse.ArgumentParser(description=description, epilog=epilog)
         parser.add_argument('-c', '--config', type=str,
-                            metavar='config',
                             default='~/.config/maxime.ini',
                             help='path to configuration file')
+
+        parser.add_argument('-v', '--verbose',
+                             default=False,
+                             action='store_true',
+                             help='enable verbose logging')
+
+        parser.add_argument('-l', '--logfile', type=str,
+                            default=('/var/tmp/maxime_%s.log' % os.getlogin()),
+                            help='path to log file (or emptystring for STDOUT)')
+
         return parser.parse_args()
 
 
@@ -56,6 +67,90 @@ class App:
         return file_path
 
 
+    @staticmethod
+    def setup_logging(verbose, filepath):
+        """
+        Setup the logging facility.
+        """
+        if filepath is not '':
+            if os.path.exists(filepath) is False:
+                os.mknod(filepath)
+
+        # You can only call this once, or others will be a noop.
+        if verbose is True:
+            logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', 
+                                datefmt='%m/%d/%Y %H:%M:%S',
+                                filename=filepath,
+                                level=logging.DEBUG)
+        else:
+            logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', 
+                                datefmt='%m/%d/%Y %H:%M:%S',
+                                filename=filepath,
+                                level=logging.INFO)
+
+        logging.info("Starting logging facility")
+
+
+class DBus:
+    """
+    Shell class for DBus-related functions.
+    """
+    BT_SERVICE = "org.bluez"
+    INTERFACE_PROPERTIES = "org.freedesktop.DBus.Properties"
+    INTERFACE_DEVICE = "org.bluez.Device1"
+    SIGNAL_PROPERTIESCHANGED = "PropertiesChanged"
+
+    @staticmethod
+    def get_normal_mac(mac):
+        """
+        Return a DBus-normalized MAC address.
+        """
+        return mac.replace(':','_')
+
+    @staticmethod
+    def get_bt_device_path(adapter, mac):
+        """
+        Return the DBus object path of a particular device.
+        """
+        mac = DBus.get_normal_mac(mac)
+        service = DBus.BT_SERVICE.replace('.', '/')
+        obj_path = "/%s/%s/dev_%s" % (service, adapter, mac)
+
+        return obj_path
+
+    @staticmethod
+    def event_handler(interface, changed_properties, signature):
+        """
+        Event handler for a change in a Bluetooth device state.
+        """
+        logging.info("%s: Change detected." % interface)
+
+        # Right now I only care about device connectivity
+        if interface != DBus.INTERFACE_DEVICE:
+            logging.info("%s: Ignorning change." % interface)
+            return
+
+        # Test for the appropriate key in the messages we will get
+        try:
+            conn_state = bool(changed_properties['Connected'])
+        except KeyError:
+            # Ignore a ServicesResolved message
+            try:
+                services_state = bool(changed_properties['ServicesResolved'])
+                logging.info("%s: Ignoring ServicesResolved message." % interface)
+                return
+            except KeyError:
+                # Some new edge case has appeared
+                logging.error("%s: Key 'Connected' not in message and not ServiceResolved???" % interface)
+                return
+            
+            logging.error("%s: Some weird error occurred." % interface)
+            return
+
+        # Deal with the connection state
+        logging.info("%s: Connected -> %s" % (interface, conn_state))
+
+
 def main():
     """
     Main program logic. Wait for dbus events and go from there.
@@ -63,14 +158,34 @@ def main():
     # Parse command-line arguments
     args = App.read_args()
 
-    # Parse config file
+    # Setup configuration structures (Note: No logging can occur before
+    # this otherwise it will break the output)
     config = App.read_config_file(App.get_config_file_path(args))
+    App.setup_logging(args.verbose, args.logfile)
     # @TODO There will probably be more configuration options to deal with.
-
-    print config.get('bluetooth', 'adapter')
 
     # DBus setup
     DBusGMainLoop(set_as_default=True)
+
+    adapter = config.get('bluetooth', 'adapter')
+    mac = config.get('bluetooth', 'device_mac')
+
+    obj_path = DBus.get_bt_device_path(adapter, mac)
+
+    bus = dbus.SystemBus()
+    dbus_bt_dev_proxy = bus.get_object(DBus.BT_SERVICE, obj_path)
+    dbus_bt_dev_proxy.connect_to_signal(DBus.SIGNAL_PROPERTIESCHANGED,
+                                   DBus.event_handler,
+                                   dbus_interface=DBus.INTERFACE_PROPERTIES)
+    
+    # Print some info about the device
+    dbus_bt_dev_properties = dbus.Interface(dbus_bt_dev_proxy,
+                                            dbus_interface=DBus.INTERFACE_PROPERTIES)
+    bt_dev_name = dbus_bt_dev_properties.Get(DBus.INTERFACE_DEVICE, 'Name')
+    print bt_dev_name
+
+    dbus_loop = GLib.MainLoop()
+    dbus_loop.run()
 
 if __name__ == "__main__":
     main()
