c8b23aa06e49db27ed2f77495c11bd914c8cf490
Grant Cohoe <grant@grantcohoe.com>
2017-05-17 22:03:56 -0400

    Made a Maxime class

--- maxime2.py
+++ maxime2.py
@@ -20,21 +20,75 @@ class App:
     Shell class for application methods (arguments, config, that sort of
     stuff).
     """
-    @staticmethod
-    def read_config_file(file_path):
-        """
-        Read configuration directives from the config file.
-        :param file_path: Path to the file that we want to read.
-        :return: ConfigParser object.
-        """
-        # Read the config
-        config = ConfigParser.RawConfigParser()
-        config.read(file_path)
-
-        return config
 
     @staticmethod
-    def read_args():
+    def send_notification(text, icon='', time=5000):
+        """
+        Send an OS notification to the user.
+        :param text: Text to display.
+        :param icon: Name of the icon to use.
+        :param time: Time the notification should live.
+        :return: None
+        """
+
+        # This blog post has a lot of good examples on how to do this.
+        # http://cheesehead-techblog.blogspot.com/2009/02/five-ways-to-make-notification-pop-up.html
+        app_name = "Maxime"
+        id_num_to_replace = 0
+        title = app_name
+
+        actions_list = ''
+        hint = ''
+
+        # Create the objects and send!
+        bus = dbus.SessionBus()
+        dbus_notify_proxy = bus.get_object(DBus.SERVICE_NOTIFICATIONS, DBus.PATH_NOTIFICATIONS)
+        dbus_notify_interface = dbus.Interface(dbus_notify_proxy, DBus.INTERFACE_NOTIFICATIONS)
+        dbus_notify_interface.Notify(app_name, id_num_to_replace, icon,
+                                     title, text, actions_list, hint, time)
+
+
+class Maxime:
+    """
+    Main application logic class.
+    """
+    MODE_ROUTE = "route"
+    MODE_CONNECT = "connect"
+    MODE_DISCONNECT = "disconnect"
+    MODE_LISTEN = "listen"
+    MODE_DAEMON = "daemon"
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        # CLI Args
+        self.args = self._setup_args(self)
+
+        # Config File
+        config_file_path = self._get_config_file_path(self)
+        self.config = self._read_config_file(self, config_file_path)
+        self._validate_config(self)
+
+        # Logging
+        self._setup_logging(self)
+
+        # Program mode
+        # This cannot run until we have setup logging!
+        self.mode = None
+        self._validate_args()
+
+    @staticmethod
+    def _validate_config(self):
+        """
+        Validate the input from a configuration file.
+        :param self: 
+        :return: 
+        """
+        pass
+
+    @staticmethod
+    def _setup_args(self):
         """
         Read arguments from the CLI.
         :return: ArgParse object.
@@ -78,82 +132,53 @@ class App:
         return parser.parse_args()
 
     @staticmethod
-    def get_config_file_path(args):
+    def _read_config_file(self, file_path):
+        """
+        Read configuration directives from the config file.
+        :param file_path: Path to the file that we want to read.
+        :return: ConfigParser object.
+        """
+        # Read the config
+        config = ConfigParser.RawConfigParser()
+        config.read(file_path)
+
+        return config
+
+    @staticmethod
+    def _get_config_file_path(self):
         """
         Return a normalized path to our configuration file.
-        :param args: ArgParse object.
         :return: String of the path to our config file.
         """
         # Normalize our path
-        file_path = args.config
+        file_path = self.args.config
         file_path = os.path.expanduser(file_path)
         file_path = os.path.abspath(file_path)
 
         # Test if the file exists
         if os.path.exists(file_path) is False:
-            raise Exception("Config file at '%s' does not exist!" % file_path)
+            self.exit_err("Config file at '%s' does not exist!" % file_path)
 
         return file_path
 
     @staticmethod
-    def setup_logging(debug, filepath):
+    def _setup_logging(self):
         """
         Setup the logging facility.
-        :param debug: Boolean of whether to be loud or not.
-        :param filepath: Log file path.
         :return: None
         """
+        log_level = logging.INFO
+
+        if self.args.debug is True:
+            log_level = logging.DEBUG
 
         # You can only call this once, or others will be a noop.
-        if debug is True:
-            logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', 
-                                datefmt='%m/%d/%Y %H:%M:%S',
-                                filename=filepath,
-                                level=logging.DEBUG)
-        else:
-            logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', 
-                                datefmt='%m/%d/%Y %H:%M:%S',
-                                filename=filepath,
-                                level=logging.INFO)
+        logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
+                            datefmt='%m/%d/%Y %H:%M:%S',
+                            filename=self.args.logfile,
+                            level=log_level)
 
         logging.info("Starting logging facility")
-
-    @staticmethod
-    def log_angry(level, message):
-        """
-        Log a message to both stdout and the log facility.
-        :param level: The logging.LEVEL
-        :param message: The message
-        :return: None
-        """
-        logging.log(level, message)
-        print message
-
-    @staticmethod
-    def send_notification(text, icon='', time=5000):
-        """
-        Send an OS notification to the user.
-        :param text: Text to display.
-        :param icon: Name of the icon to use.
-        :param time: Time the notification should live.
-        :return: None
-        """
-
-        # This blog post has a lot of good examples on how to do this.
-        # http://cheesehead-techblog.blogspot.com/2009/02/five-ways-to-make-notification-pop-up.html
-        app_name = "Maxime"
-        id_num_to_replace = 0
-        title = app_name
-
-        actions_list = ''
-        hint = ''
-
-        # Create the objects and send!
-        bus = dbus.SessionBus()
-        dbus_notify_proxy = bus.get_object(DBus.SERVICE_NOTIFICATIONS, DBus.PATH_NOTIFICATIONS)
-        dbus_notify_interface = dbus.Interface(dbus_notify_proxy, DBus.INTERFACE_NOTIFICATIONS)
-        dbus_notify_interface.Notify(app_name, id_num_to_replace, icon,
-                                     title, text, actions_list, hint, time)
 
     @staticmethod
     def exit_err(message):
@@ -163,45 +188,59 @@ class App:
         :return: None
         """
         logging.error(message)
+        logging.info("Exiting with error.")
         exit(1)
+        
+    def _validate_args(self):
+        """
+        Figure out what we should be doing based on CLI args
+        :return: 
+        """
+        # Determine what we're going to do
+        if self.args.route is not None:
+            if self.args.connect is True or self.args.disconnect is True or self.args.listen is True:
+                self.exit_err("You cannot specify --route and --connect/--disconnect/--listen")
+
+            self._set_mode(Maxime.MODE_ROUTE)
+            return
+
+        if self.args.connect is True:
+            if self.args.disconnect is True:
+                self.exit_err("You cannot specify both --connect and --disconnect.")
+
+            self._set_mode(Maxime.MODE_CONNECT)
+            return
+
+        elif self.args.disconnect is True:
+            self._set_mode(Maxime.MODE_DISCONNECT)
+            return
+        else:
+            logging.info("Starting daemon mode.")
+            if self.args.listen is True:
+                self._set_mode(Maxime.MODE_LISTEN)
+                return
+
+            self._set_mode(Maxime.MODE_DAEMON)
+            return
+
+    def _set_mode(self, mode):
+        """
+        Set the run mode of the program.
+        :return: 
+        """
+        logging.debug("Setting mode to %s" % mode)
+        self.mode = mode
+
+
 
 
 def main():
     """
     Main program logic. Wait for dbus events and go from there.
     """
-    # Parse command-line arguments
-    args = App.read_args()
+    max = Maxime()
+    logging.info("Our mode is: %s" % max.mode)
 
-    # Setup configuration structures (Note: No logging can occur before
-    # this otherwise it will break the output)
-    config = App.read_config_file(App.get_config_file_path(args))
-    App.setup_logging(args.debug, args.logfile)
-
-    # Determine what we're going to do
-    if args.route is not None:
-        if args.connect is True or args.disconnect is True or args.listen is True:
-            App.exit_err("You cannot specify --route and --connect/--disconnect/--listen")
-
-        logging.info("Routing audio stream")
-        # App code here
-
-    if args.connect is True:
-        if args.disconnect is True:
-            App.exit_err("You cannot specify both --connect and --disconnect.")
-
-        logging.info("Triggering a connection event.")
-        # App code here
-
-    elif args.disconnect is True:
-        logging.info("Triggering a disconnection event.")
-        # App code here
-
-    else:
-        logging.info("Starting daemon mode.")
-        if args.listen is True:
-            logging.info("Listening only")
-        # App code here with conditionals for listen
 
     logging.info("Reached the end, for now.")
 
